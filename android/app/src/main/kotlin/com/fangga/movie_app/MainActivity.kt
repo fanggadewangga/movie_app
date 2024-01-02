98b08a8a71d2b1d39219d1a1171d92494f1a675a
Fa'iq Arya Dewangga <fanggadewangga@gmail.com>
2024-01-02 09:55:43 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/fangga/movie_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fangga.movie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/domain/usecases/get_popular_movies_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:movie_app/domain/entities/movie.dart';
+import 'package:movie_app/domain/usecases/get_popular_movies.dart';
+import 'package:movie_app/domain/usecases/get_trending_movies.dart';
+
+import 'get_trending_movies_test.mocks.dart';
+
+void main() {
+  late GetPopularMovies usecase;
+  late MockMovieRepository mockMovieRepository;
+
+  setUp(() {
+    mockMovieRepository = MockMovieRepository();
+    usecase = GetPopularMovies(mockMovieRepository);
+  });
+
+  final tMoviesList = [
+    Movie(
+        id: 1,
+        title: "Test Movie 1",
+        overview: "Desc 1",
+        posterPath: "/image1"),
+    Movie(
+        id: 2,
+        title: "Test Movie 2",
+        overview: "Desc 2",
+        posterPath: "/image2"),
+  ];
+
+  test("should get popular movies from repository", () async {
+    // arrange
+    when(mockMovieRepository.getPopularMovies())
+        .thenAnswer((_) async => tMoviesList);
+
+    // act
+    final result = await usecase();
+
+    // assert
+    expect(result, tMoviesList);
+    verify(mockMovieRepository.getPopularMovies());
+    verifyNoMoreInteractions(mockMovieRepository);
+  });
+}
